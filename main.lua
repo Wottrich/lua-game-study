a847599abbe6f65276021ac3b66698286ff1f9a8
Lucas Cruz Wottrich <lucas.wottrich@ifood.com.br>
2022-03-01 20:46:10 -0300

    Step 3.1: Set background image

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ function love.load()
     love.window.setMode(320, 480, { resizable = false })
     love.window.setTitle("14bis vs Meteoros")
 
+    background = love.graphics.newImage("images/background.png")
+
     x, y, w, h = 20, 20, 60, 20
 end
 
@@ -24,6 +26,6 @@ end
 
 -- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.setColor(0, 0.4, 0.4)
+    love.graphics.draw(background)
     love.graphics.rectangle("fill", x, y, w, h)
 end
